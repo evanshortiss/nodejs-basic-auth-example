a9f0d88c316a1e3831e9c7a80f8ec6f90e91fdc7
Evan Shortiss <evanshortiss@gmail.com>
2020-08-20 11:58:36 +0100

    feat: use style.css, add list rendering

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const log = require('barelog')
 const auth = require('express-basic-auth')
 const exphbs  = require('express-handlebars')
-const { HTTP_PORT } = require('./config')
+const { HTTP_PORT } = require('./lib/config')
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
@@ -14,18 +14,19 @@ app.set('view engine', 'handlebars')
 // Basic authentication
 app.use(auth({
   challenge: true,
-  users: require('./users')
+  users: require('./lib/users')
 }))
 
 app.get('/', (req, res) => {
   // auth.user is appended by the basic auth module
   res.render('index.handlebars', {
-    username: req.auth.user
+    username: req.auth.user,
+    characters: require('./lib/characters')
   })
 })
 
 // Serve files, e.g style.css from public directory
-app.get(express.static(join(__dirname, 'public/')))
+app.use(express.static(join(__dirname, 'public')))
 
 app.use((err, req, res, next) => {
   log(`ERROR: An error occurred processing a request to: ${req.method} ${req.originalUrl}`)
